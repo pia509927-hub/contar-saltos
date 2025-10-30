dd5e39bdacbe64cca9bed6d5ae35a860f297b472
pia509927-hub <pia509927@gmail.com>
2025-10-30 09:38:28 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    let SALTOS = 0
+    fijar_saltos += 1
+    basic.showNumber(SALTOS)
+})
+let fijar_saltos = 0
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
